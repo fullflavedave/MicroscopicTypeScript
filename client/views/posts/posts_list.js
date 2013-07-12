d05f318943af76d1da3bdb6feb63fa2f37ee7734
Dave Allen <david.allen@meliortechnology.com>
2013-07-12 12:46:40 -0700

    Added code to NOT autopublish and to explicity enable get the posts from mongo

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,19 +1,23 @@
 var postsData = [
-    {
-        title: 'Introducing Telescope',
-        author: 'Sacha Greif',
-        url: 'http://sachagreif.com/introducing-telescope/'
-    },
-    {
-        title: 'Meteor',
-        author: 'Tom Coleman',
-        url: 'http://meteor.com'
-    },
-    {
-        title: 'The Meteor Book', author: 'Tom Coleman',
-        url: 'http://themeteorbook.com'
-    }
+  {
+    title: 'Introducing Telescope',
+    author: 'Sacha Greif',
+    url: 'http://sachagreif.com/introducing-telescope/'
+  },
+  {
+    title: 'Meteor',
+    author: 'Tom Coleman',
+    url: 'http://meteor.com'
+  },
+  {
+    title: 'The Meteor Book', author: 'Tom Coleman',
+    url: 'http://themeteorbook.com'
+  }
 ];
+
 Template.postsList.helpers({
-    posts: postsData
+  posts: function() {
+    return Posts.find();
+  }
+//  posts: postsData
 });
