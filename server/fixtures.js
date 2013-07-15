ce5fd5b874e73ed34616184dfe86bcc1bea1178f
Dave Allen <david.allen@meliortechnology.com>
2013-07-15 10:21:32 -0700

    some minor changes

--- server/fixtures.js
+++ server/fixtures.js
@@ -6,10 +6,12 @@ if (Posts.find().count() === 0) {
   });
   Posts.insert({
     title: 'Meteor',
-    author: 'Tom Coleman', url: 'http://meteor.com'
+    author: 'Tom Coleman',
+    url: 'http://meteor.com'
   });
   Posts.insert({
-    title: 'The Meteor Book', author: 'Tom Coleman',
+    title: 'The Meteor Book',
+    author: 'Tom Coleman',
     url: 'http://themeteorbook.com'
   });
 }
