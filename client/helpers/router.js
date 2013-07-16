7e0662254e7a5e0748cc3ca7e0063eb28aa5514a
Dave Allen <david.allen@meliortechnology.com>
2013-07-16 15:39:06 -0700

    Added more security to page and element viewing (before just security for Posts collection

--- client/helpers/router.js
+++ client/helpers/router.js
@@ -8,3 +8,16 @@ Meteor.Router.add({
 
   '/submit': 'postSubmit'
 });
+
+Meteor.Router.filters({
+  'requireLogin': function(page) {
+    if (Meteor.user())
+      return page;
+    else if (Meteor.loggingIn())
+      return 'loading'
+    else
+      return 'accessDenied';
+  }
+});
+
+Meteor.Router.filter('requireLogin', {only: 'postSubmit'});
