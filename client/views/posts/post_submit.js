7adc47a815c2aaf60752b178c713976685a723d0
Dave Allen <david.allen@meliortechnology.com>
2013-07-17 15:47:48 -0700

    Added more elegant error handling

--- client/views/posts/post_submit.js
+++ client/views/posts/post_submit.js
@@ -8,9 +8,16 @@ Template.postSubmit.events({
     }
 
     Meteor.call('insertPost', post, function (error, id) {
-      if (error)
-        return alert(error.reason);
-      Meteor.Router.to('postPage', id);
+      if (error) {
+        //display the error to the user
+        throwError(error.reason);
+
+        // if the error is that the post already exists, take us there
+        if (error.error === 302)
+          Meteor.Router.to('postPage', error.details)
+      } else {
+        Meteor.Router.to('postPage', id);
+      }
     });
   }
 });
