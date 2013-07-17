af06b687258dac646c4bb267730b50a21cacb7ae
Dave Allen <david.allen@meliortechnology.com>
2013-07-17 11:17:36 -0700

    Added basics for editing a post

--- client/views/posts/post_item.js
+++ client/views/posts/post_item.js
@@ -1,7 +1,10 @@
 Template.postItem.helpers({
-    domain: function() {
-        var a = document.createElement('a');
-        a.href = this.url;
-        return a.hostname;
-    }
+  ownPost: function () {
+    return this.userId == Meteor.userId();
+  },
+  domain: function () {
+    var a = document.createElement('a');
+    a.href = this.url;
+    return a.hostname;
+  }
 });
