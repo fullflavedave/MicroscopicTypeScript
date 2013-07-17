16e7a5424105f29b776d0b7f828e2ce150d9aeae
Dave Allen <david.allen@meliortechnology.com>
2013-07-17 12:41:04 -0700

    added permissions for post editing form

--- collections/posts.js
+++ collections/posts.js
@@ -1,20 +1,32 @@
 Posts = new Meteor.Collection('posts');
 
+Posts.allow({
+  update: ownsDocument,
+  remove: ownsDocument
+})
+
+Posts.deny({
+  update: function(userId, post, fieldNames) {
+    // may only edit the following threee fields:
+    return (_.without(fieldNames, 'url', 'title').length > 0);
+  }
+});
+
 Meteor.methods({
   insertPost: function(postAttributes) {
     var user = Meteor.user(),
         postWithSameLink = Posts.findOne({url: postAttributes.url});
-// ensure the user is logged in
+    // ensure the user is logged in
     if (!user)
       throw new Meteor.Error(401, "You need to login to post new stories");
-// ensure the post has a title
+    // ensure the post has a title
     if (!postAttributes.title)
       throw new Meteor.Error(422, 'Please fill in a title');
-// check that there are no previous posts with the same link
+    // check that there are no previous posts with the same link
     if (postAttributes.url && postWithSameLink) { throw new Meteor.Error(302,
         'This link has already been posted',
         postWithSameLink._id); }
-// pick out the whitelisted keys
+    // pick out the whitelisted keys
     var post = _.extend(_.pick(postAttributes, 'url', 'title', 'message'), { userId: user._id,
       author: user.username,
       submitted: new Date().getTime()
