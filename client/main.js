07a01c0d6f66d5faf4998daf760ff2540672597b
Dave Allen <david.allen@meliortechnology.com>
2013-07-19 15:19:05 -0700

    Added notifications

--- client/main.js
+++ client/main.js
@@ -3,3 +3,5 @@ Meteor.subscribe('posts');
 Deps.autorun(function () {
   Meteor.subscribe('comments', Session.get('currentPostId'));
 });
+
+Meteor.subscribe('notifications');
--- collections/comments.js
+++ collections/comments.js
@@ -22,6 +22,12 @@ Meteor.methods({
 
     Posts.update(comment.postId, {$inc: {commentsCount: 1}});
 
-    return Comments.insert(comment);
+    // create the comment, save the id
+    comment._id = Comments.insert(comment);
+
+    // now create a notificaiton, informing the user that there's been a comment
+    createCommentNotification(comment);
+
+    return comment._id;
   }
 });
