3f18ab1b96c3b6de336b78e2e825e8225a2cd0d5
Dave Allen <david.allen@meliortechnology.com>
2013-07-22 12:13:02 -0700

    Added header code, fixed some typos

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,36 +1,35 @@
-var postsData = [
-  {
-    title: 'Introducing Telescope',
-    author: 'Sacha Greif',
-    url: 'http://sachagreif.com/introducing-telescope/'
-  },
-  {
-    title: 'Meteor',
-    author: 'Tom Coleman',
-    url: 'http://meteor.com'
-  },
-  {
-    title: 'The Meteor Book', author: 'Tom Coleman',
-    url: 'http://themeteorbook.com'
-  }
-];
-
-Template.postsList.helpers({
-  posts: function() {
-    return Posts.find({}, {sort: {submitted: -1}, limit: postsHandle.limit()});
-  },
-  postsReady: function() {
-    return ! postsHandle.loading();
-  },
-  allPostsLoaded: function() {
-    return ! postsHandle.loading() &&
-        Posts.find().count() < postsHandle.loaded();
+Template.newPosts.helpers({
+  options: function () {
+    return {
+      sort: {submitted: -1},
+      handle: newPostsHandle
+    }
   }
 });
 
-Template.postsList.events({
-  'click .load-more': function(event) {
-    event.preventDefault();
-    postsHandle.loadNextPage();
+Template.bestPosts.helpers({
+  options: function () {
+    return {
+      sort: {votes: -1, submitted: -1},
+      handle: bestPostsHandle
+    }
   }
-})
+});
+
+Template.postsList.helpers({
+  posts: function () {
+    return Posts.find({}, {sort: this.sort, limit: this.handle.limit()});
+  },
+  postsReady: function () {
+    return this.handle.ready();
+  },
+  allPostsLoaded: function () {
+    return this.handle.ready() && Posts.find().count() < this.handle.loaded();
+  } });
+
+Template.postsList.events({
+  'click .load-more': function (event) {
+    event.preventDefault();
+    this.handle.loadNextPage();
+  }
+});
